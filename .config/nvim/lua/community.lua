362395e682d1d67383e63597e1e1052d55ead576
maxmya <maxmya@tutamail.com>
2025-11-11 10:06:33 +0200

    edit nvim

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -1,5 +1,6 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.utility.noice-nvim" }
+  { import = "astrocommunity.utility.noice-nvim" },
+  { import = "astrocommunity.editing-support.suda-vim" },
 }
